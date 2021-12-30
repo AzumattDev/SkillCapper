cfb64aacfbeacad8b6c207a0e770788a546abc23
AzumattDev <azumattgaming@gmail.com>
2021-12-30 03:31:12 -0500

    Write default values, add Un_Cap stuff.

--- SkillCapper.cs
+++ SkillCapper.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
@@ -19,15 +23,15 @@ namespace SkillCapper
         internal const string Author = "Azumatt";
         private const string ModGuid = "azumatt.skillcapper";
         private const string ConfigFileName = "azumatt_skillcapper_config.yaml";
-        private static string _skillConfigPath = null!;
+        internal static string _skillConfigPath = null!;
         private static SortedDictionary<string, SkillConfig> skillConfigs = new();
+        private static List<CodeInstruction> _codeInstructions = new();
+        private static List<CodeInstruction> _codeInstructionsSkillDialog = new();
+        public static MethodInfo TranspilerMethod;
+        private static Dictionary<int, int> cappedvalues = new();
 
-
-        /*private static IDeserializer deserializer = new DeserializerBuilder()
-            .WithNamingConvention(CamelCaseNamingConvention.Instance).Build();*/
-
-        private Harmony _harmony;
-        private static readonly ManualLogSource SCLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
+        private Harmony? _harmony;
+        private static readonly ManualLogSource ScLogger = BepInEx.Logging.Logger.CreateLogSource(ModName);
 
         private static readonly ConfigSync? configSync = new(ModName)
             { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
@@ -36,71 +40,85 @@ namespace SkillCapper
 
         #region UnityEvents
 
+        public static float AzuLimitSkill(Skills.Skill skill)
+        {
+            return cappedvalues.ContainsKey((int)skill.m_info.m_skill) ? cappedvalues[(int)skill.m_info.m_skill] : 100;
+        }
+
+
         private void Awake()
         {
             /*ConfigInit();*/
             _serverConfigLocked = config("General", "Force Server Config", true, "Force Server Config");
             _ = configSync?.AddLockingConfigEntry(_serverConfigLocked);
 
-            if (!File.Exists(Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName))
-            {
-                File.Create(Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName);
-            }
 
             _skillConfigPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+            if (!File.Exists(Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName))
+            {
+                File.Create(Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName).Dispose();
+                WriteDefaults.WriteDefaultValues();
+            }
 
             _harmony = new Harmony(ModGuid);
 
-            _harmony.PatchAll();
-            ReadYamlConfigFile(null!, null!);
             skillConfigData.ValueChanged += OnValChangedUpdate;
+            TranspilerMethod = AccessTools.Method(typeof(ScPlugin), nameof(AzuLimitSkill),
+                new Type[] { typeof(Skills.Skill) });
+            _codeInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0));
+            _codeInstructions.Add(new CodeInstruction(OpCodes.Call, TranspilerMethod));
 
+            _codeInstructionsSkillDialog.Add(new CodeInstruction(OpCodes.Ldloc_S, 4));
+            _codeInstructionsSkillDialog.Add(new CodeInstruction(OpCodes.Call, TranspilerMethod));
+
+
+            ReadYamlConfigFile(null!, null!);
             SetupWatcher();
+
+            _harmony.PatchAll();
         }
 
         private void OnDestroy()
         {
-            _harmony.UnpatchSelf();
+            Config.Save();
         }
 
         #endregion
 
         #region HarmonyPatches
 
-        [HarmonyPatch]
+        [HarmonyPatch(typeof(Skills.Skill), nameof(Skills.Skill.Raise))]
         private static class SKillCap
         {
-            [HarmonyPatch(typeof(Skills.Skill), nameof(Skills.Skill.Raise))]
-            [HarmonyPrefix]
             private static bool Prefix(Skills.Skill __instance)
             {
-                SCLogger.LogDebug("Skill Being Raised-----------------:" +
-                                  __instance.m_info.m_skill.ToString().ToUpper() + "a.k.a." +
+                ScLogger.LogDebug("Skill Being Raised-----------------: " +
+                                  __instance.m_info.m_skill.ToString().ToUpper() + " a.k.a. " +
                                   Localization.instance.Localize("$skill_" + __instance.m_info.m_skill.GetHashCode()));
                 foreach (KeyValuePair<string, SkillConfig> skillConfig in skillConfigs)
                 {
                     /* This mainly works only for Vanilla skills, check where possible
-                     Jotunn mods that add skills add via hash...so this would never match unless you have the has of the skill as well.
                      */
+
                     if (string.Equals(__instance.m_info.m_skill.ToString(), skillConfig.Key,
                             StringComparison.CurrentCultureIgnoreCase))
                     {
-                        SCLogger.LogDebug("Skill matched!");
-                        SCLogger.LogDebug(skillConfig.Value.Level);
+                        ScLogger.LogDebug("Skill matched!");
+                        ScLogger.LogDebug(skillConfig.Value.Level);
                         return __instance.m_level < skillConfig.Value.Level;
                     }
 
                     /* If it's a skill from SkillManager check for it using the localized name/hash.
-                     Thanks Blaxxun for making it easy to provide compatibility 
+                     Thanks Blaxxun for making it easy to provide compatibility, Jotunn will soon adopt this for compatibility 
                      */
                     if (string.Equals(skillConfig.Key,
                             Localization.instance.Localize("$skill_" + __instance.m_info.m_skill.GetHashCode()),
                             StringComparison.CurrentCultureIgnoreCase))
                     {
-                        SCLogger.LogDebug("Skill hash matched! " +
+                        ScLogger.LogDebug("Skill hash matched! " +
                                           Localization.instance.Localize("$skill_" +
                                                                          __instance.m_info.m_skill.GetHashCode()));
-                        SCLogger.LogDebug(skillConfig.Value.Level);
+                        ScLogger.LogDebug(skillConfig.Value.Level);
                         return __instance.m_level < skillConfig.Value.Level;
                     }
                 }
@@ -109,12 +127,137 @@ namespace SkillCapper
                 return true;
             }
 
-            [HarmonyPatch(typeof(Skills.Skill), nameof(Skills.Skill.Raise))]
-            [HarmonyPostfix]
-            private static void Postfix(Skills.Skill __instance)
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
-                SCLogger.LogDebug("Skillbeingraised-----------------:" +
-                                  __instance.m_info.m_skill.ToString().ToUpper());
+                List<CodeInstruction> list = new(instructions);
+                list.RemoveAt(2);
+                list.InsertRange(2, _codeInstructions);
+                list.RemoveAt(36);
+                list.InsertRange(36, _codeInstructions);
+                //list[2].operand = 100;
+                //list[35].operand = 100;
+                return list;
+            }
+        }
+
+        [HarmonyPatch(typeof(Skills), nameof(Skills.GetSkillFactor))]
+        static class Skills_GetSkillFactor_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> list = new(instructions);
+                list.RemoveAt(7);
+                list.InsertRange(7, _codeInstructions);
+                //list[7].operand = 100;
+                return list;
+            }
+        }
+
+        [HarmonyPatch(typeof(Skills), nameof(Skills.CheatRaiseSkill))]
+        static class Skills_CheatRaiseSkill_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> list = new(instructions);
+                list.RemoveAt(41);
+                list.InsertRange(41, _codeInstructions);
+                //list[41].operand = 100f;
+                return list;
+            }
+        }
+
+        [HarmonyPatch(typeof(Skills.Skill), nameof(Skills.Skill.GetLevelPercentage))]
+        static class Skills_SkillGetLevelPercentage_Patch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> list = new(instructions);
+                list.RemoveAt(2);
+                list.InsertRange(2, _codeInstructions);
+                // list[2].operand = 100;
+                return list;
+            }
+        }
+
+        [HarmonyPatch(typeof(SkillsDialog), nameof(SkillsDialog.Setup))]
+        static class SkillsDialog_SkillSetUp_Patch
+        {
+            static void Prefix(SkillsDialog __instance, ref Player player)
+            {
+                /* Make sure to sync the values each time a player opens the menu */
+                cappedvalues.Clear();
+                foreach (Skills.Skill? skill in player.GetSkills().GetSkillList())
+                {
+                    string[] split = skill.m_info.m_description.Split('_');
+
+                    if (split.Length < 2) continue;
+
+                    string name = split[1].ToLower();
+
+                    if (!skillConfigs.ContainsKey(name)) continue;
+
+                    cappedvalues.Add((int)skill.m_info.m_skill, (int)skillConfigs[name].Level);
+                }
+            }
+
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                List<CodeInstruction> list = new(instructions);
+                list.RemoveAt(138);
+                list.InsertRange(138, _codeInstructionsSkillDialog);
+                list.RemoveAt(149);
+                list.InsertRange(149, _codeInstructionsSkillDialog);
+                /*list[138].operand = 100;
+                list[148].operand = 100;*/
+                return list;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), nameof(Player.OnSpawned))]
+        static class Player_SetLocalPlayer_Patch
+        {
+            static void Postfix(Player __instance)
+            {
+                /* Make sure to sync the values each time a player spawns */
+                cappedvalues.Clear();
+                foreach (Skills.Skill? skill in __instance.GetSkills().GetSkillList())
+                {
+                    string[] split = skill.m_info.m_description.Split('_');
+
+                    if (split.Length < 2) continue;
+
+                    string name = split[1].ToLower();
+
+                    if (!skillConfigs.ContainsKey(name)) continue;
+
+                    cappedvalues.Add((int)skill.m_info.m_skill, (int)skillConfigs[name].Level);
+                    ScLogger.LogDebug(
+                        $"------------------------------SKILL NAME VAR: {name}, SKILL {skill.m_info.m_skill}, SKILLCONFIG LEVEL {(int)skillConfigs[name].Level}");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(EnvMan), nameof(EnvMan.GetCurrentDay))]
+        static class EnvMan_GetCurrentDay_Patch
+        {
+            static void Postfix(EnvMan __instance)
+            {
+                /* For players that might not die as much or at all. Update the values in their list every morning in game */
+                if (Player.m_localPlayer == null) return;
+                cappedvalues.Clear();
+                foreach (Skills.Skill? skill in Player.m_localPlayer.GetSkills().GetSkillList())
+                {
+                    string[] split = skill.m_info.m_description.Split('_');
+
+                    if (split.Length < 2) continue;
+
+                    string name = split[1].ToLower();
+
+                    if (!skillConfigs.ContainsKey(name)) continue;
+
+                    cappedvalues.Add((int)skill.m_info.m_skill, (int)skillConfigs[name].Level);
+                }
             }
         }
 
@@ -172,7 +315,7 @@ namespace SkillCapper
             if (!File.Exists(_skillConfigPath)) return;
             try
             {
-                SCLogger.LogWarning("ReadYamlConfigFile called");
+                ScLogger.LogDebug("ReadYamlConfigFile called");
                 StreamReader file = File.OpenText(_skillConfigPath);
                 IDeserializer deserializer = new DeserializerBuilder()
                     .WithNamingConvention(CamelCaseNamingConvention.Instance)
@@ -185,8 +328,8 @@ namespace SkillCapper
             }
             catch
             {
-                SCLogger.LogError($"There was an issue loading your {ConfigFileName}");
-                SCLogger.LogError("Please check your config entries for spelling and format!");
+                ScLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                ScLogger.LogError("Please check your config entries for spelling and format!");
             }
         }
 
@@ -194,7 +337,7 @@ namespace SkillCapper
         {
             IDeserializer deserializer = new DeserializerBuilder()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-            SCLogger.LogWarning("OnValChanged called");
+            ScLogger.LogDebug("OnValChanged called");
             try
             {
                 skillConfigs = new SortedDictionary<string, SkillConfig>(
@@ -202,16 +345,13 @@ namespace SkillCapper
                     new Dictionary<string, SkillConfig>());
                 foreach (SkillConfig skillConfig in skillConfigs.Values)
                 {
-                    // yamldotnet helpfully nulls fields if empty
-                    // ReSharper disable ConstantNullCoalescingCondition
                     SkillConfig skillConfig1 = skillConfig;
-                    skillConfig1.Level ??= new float();
-                    // ReSharper restore ConstantNullCoalescingCondition
+                    skillConfig1.Level ??= new int();
                 }
             }
             catch (Exception e)
             {
-                SCLogger.LogError($"Failed to deserialize skillConfig: {e}");
+                ScLogger.LogError($"Failed to deserialize skillConfig: {e}");
             }
         }
 
--- SkillConfig.cs
+++ SkillConfig.cs
@@ -1,12 +1,63 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BepInEx;
 using JetBrains.Annotations;
 using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace SkillCapper
 {
-    public struct SkillConfig 
+    public struct SkillConfig
     {
         [YamlMember(Alias = "level", ApplyNamingConventions = false)]
-        public float? Level { get; set; }
+        public int? Level { get; set; }
+    }
+
+    internal static class WriteDefaults
+    {
+        private static readonly Dictionary<string, SkillConfig> ListSkillsDefault = new();
+
+        internal static void WriteDefaultValues()
+        {
+            ListSkillsDefault.Add("swords", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("knives", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("club", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("polearms", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("spears", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("blocking", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("axes", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("bows", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("unarmed", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("pickaxes", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("woodcutting", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("sneak", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("swim", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("run", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("jump", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("tenacity", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("vitality", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("packhorse", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("evasion", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("building", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("cooking", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("cartography", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("fitness", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("athletics", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("gathering", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("sailing", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("discipline", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("abjuration", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("alteration", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("conjuration", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("evocation", new SkillConfig { Level = 100 });
+            ListSkillsDefault.Add("illusion", new SkillConfig { Level = 100 });
+
+            ISerializer serializer = new SerializerBuilder().DisableAliases()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+            string yaml = serializer.Serialize(ListSkillsDefault);
+            File.WriteAllText(ScPlugin._skillConfigPath, yaml);
+        }
     }
 }
